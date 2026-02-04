c9ae79e1ee1e952be9a74878a8c524f896de4c67
Theos <loiuy538@gmail.com>
2026-02-04 19:18:03 +0100

    Revert "Added selection clearing"
    This reverts commit 46fd01056405ad83e74a38d0cfa9682136123c49.

--- src/EditorFeatures/Core/Editor/GoToAdjacentMemberCommandHandler.cs
+++ src/EditorFeatures/Core/Editor/GoToAdjacentMemberCommandHandler.cs
@@ -93,7 +93,6 @@ internal sealed class GoToAdjacentMemberCommandHandler(IOutliningManagerService 
         if (targetPosition != null)
         {
             args.TextView.TryMoveCaretToAndEnsureVisible(new SnapshotPoint(subjectBuffer.CurrentSnapshot, targetPosition.Value), _outliningManagerService);
-            args.TextView.Selection.Clear();
         }
 
         return true;
